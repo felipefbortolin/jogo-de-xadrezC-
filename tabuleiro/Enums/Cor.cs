d4559d8fd653809c632358c50e60fd5a0f7545b0
Felipe Bortolin <felipebortolin@gmail.com>
2021-07-10 03:38:39 -0300

    Adição da Classe Tabuleiro, Peca e Cor

---
+++ tabuleiro/Enums/Cor.cs
@@ -0,0 +1,11 @@
+namespace tabuleiro.Enums {
+    enum Cor{
+        Branca,
+        Preta,
+        Amarela,
+        Azul,
+        Vermelha,
+        Verde,
+        Laranja
+    }
+ }
---
+++ tabuleiro/Peca.cs
@@ -0,0 +1,20 @@
+using tabuleiro.Enums;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovimentos { get; protected set; }
+        public Tabuleiro Tabuleiro { get; set; }
+        
+        public Peca(Posicao posicao, Cor cor, Tabuleiro tabuleiro)
+        {
+            Posicao = posicao;
+            Cor = cor;
+            Tabuleiro = tabuleiro;
+            QteMovimentos = 0;
+        }
+    }
+}
