193e0082dbb2e94d73ccfd37a56c91026f9e9f8c
Felipe Bortolin <felipebortolin@gmail.com>
2021-07-10 04:48:26 -0300

    Implementando cores nas peças classe Tela

--- Program.cs
+++ Program.cs
@@ -10,21 +10,21 @@ namespace XadrezC_
     {
         static void Main(string[] args)
         {
-            /*try{
+            try{
             Tabuleiro tab = new Tabuleiro(8,8);
 
             tab.ColocarPeca(new Torre(tab,Cor.Preta),new Posicao(0,0));
             tab.ColocarPeca(new Torre(tab,Cor.Preta),new Posicao(1,3));
             tab.ColocarPeca(new Rei(tab,Cor.Preta),new Posicao(0,4));
 
+             tab.ColocarPeca(new Torre(tab,Cor.Branca),new Posicao(3,5));
+
             Tela.ImprimirTabuleiro(tab);
             //Console.WriteLine("Posição: "+tab);
             }catch(TabuleiroException e){
                 System.Console.WriteLine(e.Message);
-            }*/
-            PosicaoXadrez pos = new PosicaoXadrez('c',7);
-            System.Console.WriteLine(pos);
-            System.Console.WriteLine(pos.ToPosicao());
+            }
+           
         }
     }
 }
--- Tela.cs
+++ Tela.cs
@@ -1,5 +1,6 @@
 using System;
 using tabuleiro;
+using tabuleiro.Enums;
 
 namespace XadrezC_
 {
@@ -8,16 +9,34 @@ namespace XadrezC_
        public static void ImprimirTabuleiro(Tabuleiro tab){
            for (int i = 0; i < tab.Linhas; i++)
            {
+               System.Console.Write(8 - i + " ");
                for (int j = 0; j < tab.Colunas; j++)
                {
                    if (tab.Peca(i,j) == null)
                    {
                        System.Console.Write("- ");
+                   }else{
+                       Tela.ImprimirPeca(tab.Peca(i,j));
+                   System.Console.Write(" ");
                    }
-                   System.Console.Write(tab.Peca(i,j)+" ");
                }
                System.Console.WriteLine();
            }
+           System.Console.WriteLine("  a b c d e f g h");
        }
+
+       public static void ImprimirPeca(Peca peca){
+            if (peca.Cor == Cor.Branca)
+            {
+                System.Console.Write(peca);
+            }
+            else{
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                System.Console.Write(peca);
+                Console.ForegroundColor = aux;
+            }
+
+        }
     }
 }
