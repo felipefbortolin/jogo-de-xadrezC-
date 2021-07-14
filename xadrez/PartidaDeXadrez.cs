65c596c619dcb20c5200a4e9ae2044fdc0218311
Felipe Bortolin <felipebortolin@gmail.com>
2021-07-14 07:28:24 -0300

    Jogada especial roque pequeno e grande

--- xadrez/PartidaDeXadrez.cs
+++ xadrez/PartidaDeXadrez.cs
@@ -38,6 +38,27 @@ namespace xadrez
             {
                 capturadas.Add(pecaCapturada);
             }
+
+            //#Jogada especial Roque pequeno
+            if (p is Rei && destino.Coluna == origem.Coluna + 2)
+            {
+                Posicao origemT = new Posicao(origem.Linha, origem.Coluna + 3);
+                Posicao destinoT = new Posicao(origem.Linha, origem.Coluna + 1);
+                Peca T = Tab.RetirarPeca(origemT);
+                T.IncrementarQteMovimentos();
+                Tab.ColocarPeca(T, destinoT);
+            }
+
+            //#Jogada especial Roque Grande
+            if (p is Rei && destino.Coluna == origem.Coluna - 2)
+            {
+                Posicao origemT = new Posicao(origem.Linha, origem.Coluna - 4);
+                Posicao destinoT = new Posicao(origem.Linha, origem.Coluna - 1);
+                Peca T = Tab.RetirarPeca(origemT);
+                T.IncrementarQteMovimentos();
+                Tab.ColocarPeca(T, destinoT);
+            }
+
             return pecaCapturada;
         }
 
@@ -51,6 +72,24 @@ namespace xadrez
                 capturadas.Remove(pecaCapturada);
             }
             Tab.ColocarPeca(p, origem);
+            //#Jogada especial Roque pequeno
+            if (p is Rei && destino.Coluna == origem.Coluna +2)
+            {
+                Posicao origemT = new Posicao(origem.Linha,origem.Coluna+3);
+                Posicao destinoT = new Posicao(origem.Linha, origem.Coluna+1);
+                Peca T =  Tab.RetirarPeca(destinoT);
+                T.IncrementarQteMovimentos();
+                Tab.ColocarPeca(T,origemT);
+            }
+            //#Jogada especial Roque pequeno
+            if (p is Rei && destino.Coluna == origem.Coluna -2)
+            {
+                Posicao origemT = new Posicao(origem.Linha,origem.Coluna-4);
+                Posicao destinoT = new Posicao(origem.Linha, origem.Coluna-1);
+                Peca T =  Tab.RetirarPeca(destinoT);
+                T.IncrementarQteMovimentos();
+                Tab.ColocarPeca(T,origemT);
+            }
         }
 
         public void RealizaJogada(Posicao origem, Posicao destino)
@@ -229,7 +268,7 @@ namespace xadrez
             ColocarNovaPeca('b', 1, new Cavalo(Tab, Cor.Branca));
             ColocarNovaPeca('c', 1, new Bispo(Tab, Cor.Branca));
             ColocarNovaPeca('d', 1, new Dama(Tab, Cor.Branca));
-            ColocarNovaPeca('e', 1, new Rei(Tab, Cor.Branca));
+            ColocarNovaPeca('e', 1, new Rei(Tab, Cor.Branca, this));
             ColocarNovaPeca('f', 1, new Bispo(Tab, Cor.Branca));
             ColocarNovaPeca('g', 1, new Cavalo(Tab, Cor.Branca));
             ColocarNovaPeca('h', 1, new Torre(Tab, Cor.Branca));
@@ -246,7 +285,7 @@ namespace xadrez
             ColocarNovaPeca('b', 8, new Cavalo(Tab, Cor.Preta));
             ColocarNovaPeca('c', 8, new Bispo(Tab, Cor.Preta));
             ColocarNovaPeca('d', 8, new Dama(Tab, Cor.Preta));
-            ColocarNovaPeca('e', 8, new Rei(Tab, Cor.Preta));
+            ColocarNovaPeca('e', 8, new Rei(Tab, Cor.Preta, this));
             ColocarNovaPeca('f', 8, new Bispo(Tab, Cor.Preta));
             ColocarNovaPeca('g', 8, new Cavalo(Tab, Cor.Preta));
             ColocarNovaPeca('h', 8, new Torre(Tab, Cor.Preta));
--- xadrez/Rei.cs
+++ xadrez/Rei.cs
@@ -4,8 +4,10 @@ namespace xadrez
 {
     class Rei : Peca
     {
-        public Rei(Tabuleiro tabuleiro, Cor cor) : base(tabuleiro, cor)
+        private PartidaDeXadrez partida;
+        public Rei(Tabuleiro tabuleiro, Cor cor, PartidaDeXadrez partida) : base(tabuleiro, cor)
         {
+            this.partida = partida;
         }
 
         public override string ToString()
@@ -19,51 +21,99 @@ namespace xadrez
             return p == null || p.Cor != Cor;
         }
 
+        private bool TesteTorreParaRoque(Posicao pos)
+        {
+            Peca p = Tabuleiro.Peca(pos);
+            return p != null && p is Torre && p.Cor == Cor && p.QteMovimentos == 0;
+        }
+
         public override bool[,] MovimentosPossiveis()
         {
             bool[,] mat = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
-            Posicao pos = new Posicao(0,0);
+            Posicao pos = new Posicao(0, 0);
 
             //cima
-            pos.DefinirValores(Posicao.Linha -1, Posicao.Coluna);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
-                mat[pos.Linha,pos.Coluna] = true;
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
             }
             //ne
-            pos.DefinirValores(Posicao.Linha -1, Posicao.Coluna +1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
-                mat[pos.Linha,pos.Coluna] = true;
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
             }
             //direita
-            pos.DefinirValores(Posicao.Linha, Posicao.Coluna +1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
-                mat[pos.Linha,pos.Coluna] = true;
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
             }
-              //se
-            pos.DefinirValores(Posicao.Linha +1, Posicao.Coluna +1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
-                mat[pos.Linha,pos.Coluna] = true;
+            //se
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna + 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
             }
-              //baixo
-            pos.DefinirValores(Posicao.Linha +1, Posicao.Coluna);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
-                mat[pos.Linha,pos.Coluna] = true;
+            //baixo
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
             }
-              //so
-            pos.DefinirValores(Posicao.Linha +1, Posicao.Coluna-1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
-                mat[pos.Linha,pos.Coluna] = true;
+            //so
+            pos.DefinirValores(Posicao.Linha + 1, Posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
             }
-              //esquerda
-            pos.DefinirValores(Posicao.Linha, Posicao.Coluna-1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
-                mat[pos.Linha,pos.Coluna] = true;
+            //esquerda
+            pos.DefinirValores(Posicao.Linha, Posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
             }
-              //no
-            pos.DefinirValores(Posicao.Linha -1, Posicao.Coluna -1);
-            if(Tabuleiro.PosicaoValida(pos) && PodeMover(pos)){
-                mat[pos.Linha,pos.Coluna] = true;
+            //no
+            pos.DefinirValores(Posicao.Linha - 1, Posicao.Coluna - 1);
+            if (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                mat[pos.Linha, pos.Coluna] = true;
             }
+
+            //#JOGADA ESPECIAL ROQUE
+            if (QteMovimentos == 0 && !partida.Xeque)
+            {
+                //#Roque pequeno 
+                Posicao posT1 = new Posicao(Posicao.Linha, Posicao.Coluna + 3);
+                if (TesteTorreParaRoque(posT1))
+                {
+                    Posicao p1 = new Posicao(Posicao.Linha, Posicao.Coluna + 1);
+                    Posicao p2 = new Posicao(Posicao.Linha, Posicao.Coluna + 2);
+                    if (Tabuleiro.Peca(p1) == null && Tabuleiro.Peca(p2) == null)
+                    {
+                        mat[Posicao.Linha, Posicao.Coluna + 2] = true;
+                    }
+                }
+            }
+
+            //#JOGADA ESPECIAL ROQUE GRANDE
+            if (QteMovimentos == 0 && !partida.Xeque)
+            {
+                //#Roque Grande 
+                Posicao posT2 = new Posicao(Posicao.Linha, Posicao.Coluna - 4);
+                if (TesteTorreParaRoque(posT2))
+                {
+                    Posicao p1 = new Posicao(Posicao.Linha, Posicao.Coluna - 1);
+                    Posicao p2 = new Posicao(Posicao.Linha, Posicao.Coluna - 2);
+                    Posicao p3 = new Posicao(Posicao.Linha, Posicao.Coluna - 3);
+                    if (Tabuleiro.Peca(p1) == null && Tabuleiro.Peca(p2) == null && Tabuleiro.Peca(p3) == null)
+                    {
+                        mat[Posicao.Linha, Posicao.Coluna - 2] = true;
+                    }
+                }
+            }
+
             return mat;
         }
     }
